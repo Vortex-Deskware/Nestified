a787f4d0a19d67ca755a67e698115f5f2ccd86cc
Axel <kashtech.t@gmail.com>
2024-08-15 14:00:40 -0700

    Updated nestified

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, 'assets/icons/png/icon512.png'),
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    autoHideMenuBar: true,
   });
 
   win.loadURL("https://home.nest.com");
-
   Menu.setApplicationMenu(null);
 }
 
